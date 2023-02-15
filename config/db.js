33035de21416d3ae7b9dc4dae72f800a5967c9fd
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-02-15 23:02:37 +0530

    15-02-2023

---
+++ config/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+function connectDB() {
+    // Database connection 🥳
+    mongoose.set('strictQuery',true); 
+
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, { useNewUrlParser: true,  useUnifiedTopology: true});
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected 🥳🥳🥳🥳');
+    })
+}
+
+// mIAY0a6u1ByJsWWZ
+
+module.exports = connectDB;
